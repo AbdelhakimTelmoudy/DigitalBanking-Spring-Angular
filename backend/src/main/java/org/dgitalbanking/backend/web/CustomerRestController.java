7773f17ff0e2d6e92f86e0b827dff63734413106
abdelhakim telmoudy <telmoudy.abdelhakim@gmail.com>
2024-05-22 14:37:26 +0200

    first commit

---
+++ backend/src/main/java/org/dgitalbanking/backend/web/CustomerRestController.java
@@ -0,0 +1,43 @@
+package org.dgitalbanking.backend.web;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dgitalbanking.backend.dtos.CustomerDTO;
+import org.dgitalbanking.backend.exceptions.CustomerNotFoundException;
+import org.dgitalbanking.backend.services.BankAccountServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@CrossOrigin("*")
+public class CustomerRestController {
+    private BankAccountServiceImpl bankAccountService;
+    @GetMapping("/customers")
+    public List<CustomerDTO> customers(){
+        return bankAccountService.listCustomers();
+    }
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
+        return bankAccountService.searchCustomers("%"+keyword+"%");
+    }
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException, CustomerNotFoundException {
+        return bankAccountService.getCustomer(customerId);
+    }
+    @PostMapping("/customers")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+    @PutMapping("/customers/{customerId}")
+    public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO){
+        customerDTO.setId(customerId);
+        return bankAccountService.updateCustomer(customerDTO);
+    }
+    @DeleteMapping("/customers/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+        bankAccountService.deleteCustomer(id);
+    }
+}
